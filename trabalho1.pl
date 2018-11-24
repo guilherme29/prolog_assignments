d5055f7aae0f3b0ad6c45fb508a48ba84820c1d9
Guilherme Martins <gui.amado29@gmail.com>
2018-11-24 18:25:53 +0000

    it looks like scalepoly is finally working, requires further testing.."

--- trabalho1.pl
+++ trabalho1.pl
@@ -1,6 +1,6 @@
+/* definitions */
 pvars([a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]).
 pvar(X) :- pvars(V), member(X,V).
-
 
 power(X):-pvar(X),!.
 power(X^Y):-pvar(X),integer(Y),Y>1,!.
@@ -18,7 +18,7 @@ polynomial(M):-monomial(M),!.
 polynomial(P+M):-monomial(M),polynomial(P),!.
 polynomial(P-M):-monomial(-M),polynomial(P),!.
 
-
+/* actual predicates */
 poly2list(P,L):-reverse(X,L),poly2listaux(P,X),!.
 poly2listaux(M,[M]):- monomial(M),!.
 poly2listaux(P+0,[P]).
@@ -26,11 +26,6 @@ poly2listaux(0+P,[P]).
 poly2listaux(Y-X,[-X|Y1]):-poly2listaux(Y,Y1),!.
 poly2listaux(Y+X,[X|Y1]):-poly2listaux(Y,Y1),!.
 
-
-
-simmon(1*P,P):-power(P),!.
-simmon(0*_,0):-!.
-simmon(M,M).
 
 simpoly(M,M2):-monomial(M),simmon(M,M2),!.
 simpoly(P+0,P):-!.
@@ -41,22 +36,21 @@ simpoly(P+M,P2+M3):-
     addmonomial(M,M2,M3).
 simpoly(P+M,P2+M2):-simpoly(P,P2),simmon(M,M2),!.
 
+simmon(1*P,P):-power(P),!.
+simmon(0*_,0):-!.
+simmon(M,M).
+
 monparts(X^N,1,X^N):-power(X^N),!.
 monparts(K*P,K,P):-number(K),!.
 monparts(K,K,indep):-number(K),!.
 monparts(X,1,X):-pvar(X),!.
 
-delmonomial(M,X,M,0):-
-    monomial(M),monparts(M,_,X),!.
-delmonomial(M+M2,X,M,M2):-
-    monomial(M2),monomial(M),monparts(M,_,X),!.
-delmonomial(P+M,X,M,P):-
-    monomial(M),monparts(M,_,X),!.
-delmonomial(P+M2,X,M,P2+M2):-
-    delmonomial(P,X,M,P2).
+delmonomial(M,X,M,0):- monomial(M),monparts(M,_,X),!.
+delmonomial(M+M2,X,M,M2):- monomial(M2),monomial(M),monparts(M,_,X),!.
+delmonomial(P+M,X,M,P):- monomial(M),monparts(M,_,X),!.
+delmonomial(P+M2,X,M,P2+M2):- delmonomial(P,X,M,P2).
 
-addmonomial(K1,K2,K3):-
-    number(K1),number(K2),!,K3 is K1+K2.
+addmonomial(K1,K2,K3):- number(K1),number(K2),!,K3 is K1+K2.
 addmonomial(M1,M2,M3):-
     monparts(M1,K1,XExp),
     monparts(M2,K2,XExp),
@@ -79,14 +73,14 @@ simpoly_list(V,X):- list2poly(V,Y), simpoly(Y,Z), poly2list(Z,X).
 addpoly(X,Y,Z):- poly2list(X+Y,L), simpoly_list(L,T), list2poly(T,Z), !.
 
 %muliplica uma lista por um numero N
+
 scalelist(0, _, [0]).
-scalelist(N, [X|[]], [N*X]).
-scalelist(N, [X|T], [R|T2]):- number(N), scalemonomial(X,N,R), scalelist(N, T, T2), !.
+scalelist(N, [X|[]], [R]):- number(N), scalemonomial(X,N,R),!.
+scalelist(N, [X|T], [R|T2]):- number(N), scalemonomial(X,N,R), scalelist(N, T, T2),!.
 
 scalemonomial(K1,K2,K3):-
     number(K1),number(K2),!,K3 is K1*K2.
-%M2 e a constante aqui
-scalemonomial(M1,M2,M3):-
+scalemonomial(M1,M2,M3):- %M2 e a constante
     monomial(M1),
     monparts(M1,K1,XExp),
     number(M2),
