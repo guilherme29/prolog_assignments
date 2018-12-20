58eb80ec4cc0fd3c59837ab48b406dbf5aa47861
Zeterd <jpsousa13@gmail.com>
2018-12-20 17:32:01 +0000

    simplify almost perfect

--- trabalho1.pl
+++ trabalho1.pl
@@ -1,47 +1,45 @@
 /*
-    Grupo: nº4
-    Membros:
-      Guilherme Martins Amado   up201608528
-      João Vasco Semblano       up201605518
-      Jose Pedro Sousa          up201503443
+     Grupo: nº4
+     Membros:
+       Guilherme Martins Amado   up201608528
+       João Vasco Semblano       up201605518
+       Jose Pedro Sousa          up201503443
 
-    Codigo baseado nos slides das aulas teoricas
+     Codigo baseado nos slides das aulas teoricas
 
-*/
+ */
 
-/* definitions */
-pvars([a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]).
-pvar(X) :- pvars(V), member(X,V).
+ /* definitions */
+ pvars([a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]).
+ pvar(X) :- pvars(V), member(X,V).
 
-power(X):-pvar(X),!.
-power(X^Y):-pvar(X),integer(Y),Y>1,!.
-power(-X^Y):-pvar(X),integer(Y),Y>1,!.
+ power(X):-pvar(X),!.
+ power(X^Y):-pvar(X),integer(Y),Y>1,!.
+ power(-X^Y):-pvar(X),integer(Y),Y>1,!.
 
-coefficient(K):-number(K).
+ coefficient(K):-number(K).
 
-monomial(X):-pvar(X),!.
-monomial(-X):-pvar(X),!.
-monomial(N):-number(N),!.
-monomial(-N):-number(N),!.
-monomial(X):-power(X),!.
-monomial(K*X):-coefficient(K),power(X),!.
-monomial(K*X):-coefficient(K),pvar(X),!.
+ monomial(X):-pvar(X),!.
+ monomial(-X):-pvar(X),!.
+ monomial(N):-number(N),!.
+ monomial(-N):-number(N),!.
+ monomial(X):-power(X),!.
+ monomial(K*X):-coefficient(K),power(X),!.
+ monomial(K*X):-coefficient(K),pvar(X),!.
 
-polynomial(M):-monomial(M),!.
-polynomial(-M):-monomial(-M),!.
-polynomial(P+M):-monomial(M),polynomial(P),!.
-polynomial(P-M):-monomial(-M),polynomial(P),!.
+ polynomial(M):-monomial(M),!.
+ polynomial(-M):-monomial(-M),!.
+ polynomial(P+M):-monomial(M),polynomial(P),!.
+ polynomial(P-M):-monomial(-M),polynomial(P),!.
 
-/* actual predicates */
-%transforma um polinomio em uma lista de monomios
-poly2list(P,L):-reverse(X,L),poly2listaux(P,X),!.
-poly2listaux(M,[M]):- monomial(M),!.
-poly2listaux(P+0,[P]).
-poly2listaux(0+P,[P]).
-poly2listaux(Y-X,[-X|Y1]):-poly2listaux(Y,Y1),!.
+ /* actual predicates */
+ %transforma um polinomio em uma lista de monomios
+ poly2list(P,L):-reverse(X,L),poly2listaux(P,X),!.
+ poly2listaux(M,[M]):- monomial(M),!.
+ poly2listaux(P+0,[P]).
+ poly2listaux(0+P,[P]).
+ poly2listaux(Y-X,[-X|Y1]):-poly2listaux(Y,Y1),!.
 poly2listaux(Y+X,[X|Y1]):-poly2listaux(Y,Y1),!.
-
-%simplifica um polinomio
 
 simpoly(P,P):-
  aux_simpoly(P,P2),
@@ -50,10 +48,21 @@ simpoly(P,P3):-
  aux_simpoly(P,P2),
  simpoly(P2,P3),!.
 
-minus_op(M1,M2):- number(M1), M1 < 0, M2 is M1*(-1),!.
-minus_op(-M,M):-!.
-minus_op(-1*M,M):-!.
-minus_op(M1,M1).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% aux_simpoly(_*0,0):-!.
+% aux_simpoly(0*_,0):-!.
+% aux_simpoly(M1*M2,M3):-
+%    mulmonomial(M1,M2,M3).
+%aux_simpoly(P*M,P2*M2):-aux_simpoly(P,P2),aux_simpoly(M,M2).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+takeminus(M1,M2):- number(M1), M1 < 0, M2 is M1*(-1),!.
+takeminus(-M,M):-!.
+takeminus(-1*M,M):-!.
+takeminus(M1,M1).
 
 aux_simpoly(P-0,P):-!.
 aux_simpoly(0-M,-M):-monomial(M),!.
@@ -65,7 +74,7 @@ aux_simpoly(P-M,M3):-
 aux_simpoly(P-M,P2-R):-
  monparts(M,_,XExp),
  delmonomial(P,XExp,M2,P2),
- submonomial(M2,M,M3), minus_op(M3,R).
+ submonomial(M2,M,M3), takeminus(M3,R).
 
 
 aux_simpoly(P-M,P2-M3):-
@@ -79,12 +88,12 @@ aux_simpoly(P+0,P):-!.
 aux_simpoly(0+M,M):-monomial(M),!.
 
 aux_simpoly(P+M,P2-R):-
-   number(M),
+  number(M),
    monparts(M,_,XExp),
    delmonomial(P,XExp,M2,P2),
    addmonomial(M,M2,M3),
    M3 < 0,
-   minus_op(M3,R).
+   takeminus(M3,R).
 
 aux_simpoly(P+M,P2+M3):-
    monparts(M,_,XExp),
@@ -124,23 +133,15 @@ delmonomial(P-M,X,NM,P):-
 
 
 delmonomial(M-K*M2,X,M,NK*M2):-
-   monomial(M2),
-   monomial(M),
-   monparts(M,_,X), NK is -K,!.
-
+monomial(M2),monomial(M),monparts(M,_,X), NK is -K,!.
 delmonomial(P-M,X,-M,P):-
-   monomial(M),
-   monparts(M,_,X),!.
+monomial(M),monparts(M,_,X),!.
 
 
 delmonomial(M-M2,X,M,-M2):-
-   monomial(M2),
-   monomial(M),
-   monparts(M,_,X),!.
-
+monomial(M2),monomial(M),monparts(M,_,X),!.
 delmonomial(P-M,X,-M,P):-
-   monomial(M),
-   monparts(M,_,X),!.
+monomial(M),monparts(M,_,X),!.
 
 delmonomial(P-M2,X,M,P2-M2):-
  delmonomial(P,X,M,P2).
@@ -152,74 +153,120 @@ delmonomial(P+M,X,M,P):-
 delmonomial(P+M2,X,M,P2+M2):-
  delmonomial(P,X,M,P2).
 
- addmonomial(-K1,K2,K3):-
-   K3 is K2-K1.
- addmonomial(K1,K2,K3):-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+mulmonomial(K1,K2,NK1*NK2):-
+  not(number(K2)),
+  takeminus(K2,NK2),
+  NK2 \= K2,!,
+  NK1 is K1*(-1).
+
+mulmonomial(K1,K2,NK2*NK1):-
+  not(number(K1)),
+  takeminus(K1,NK1),
+  NK1 \= K1,!,
+  NK2 is K2*(-1).
+
+mulmonomial(K1,K2,K3):-
+ number(K1),number(K2),!,
+ K3 is K1*K2.
+mulmonomial(K1,M2,M3):-
+ number(K1),
+ monparts(M2,K2,XExp),
+ K3 is K1*K2,
+ aux_mulmonomial(K3,XExp,M3),!.
+mulmonomial(M1,K2,M3):-
+ number(K2),
+ monparts(M1,K1,XExp),
+ K3 is K1*K2,
+ aux_mulmonomial(K3,XExp,M3),!.
+mulmonomial(M1,M2,M3):-
+ monparts(M1,K1,XExp),
+ monparts(M2,K2,XExp),
+ K3 is K1+K2,
+ exp_mulmonomial(K3,XExp,M3).
+
+exp_mulmonomial(K,indep,K):-!.
+exp_mulmonomial(0,_,1):-!.
+exp_mulmonomial(1,XExp,XExp):-!.
+exp_mulmonomial(K,XExp,XExp^K).
+
+aux_mulmonomial(K,indep,K):-!.
+aux_mulmonomial(0,_,0):-!.
+aux_mulmonomial(1,XExp,XExp):-!.
+aux_mulmonomial(K,XExp,K*XExp).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+addmonomial(-K1,K2,K3):-
+  K3 is K2-K1.
+addmonomial(K1,K2,K3):-
+ number(K1),number(K2),!,
+ K3 is K1+K2.
+addmonomial(M1,M2,M3):-
+   monparts(M1,K1,XExp),
+   monparts(M2,K2,XExp),
+   K3 is K1+K2,
+   aux_addmonomial(K3,XExp,M3).
+
+aux_addmonomial(K,indep,K):-!.
+aux_addmonomial(0,_,0):-!.
+aux_addmonomial(1,XExp,XExp):-!.
+aux_addmonomial(K,XExp,K*XExp).
+
+submonomial(-K1,K2,K3):-
   number(K1),number(K2),!,
-  K3 is K1+K2.
- addmonomial(M1,M2,M3):-
-    monparts(M1,K1,XExp),
-    monparts(M2,K2,XExp),
-    K3 is K1+K2,
-    aux_addmonomial(K3,XExp,M3).
-
- aux_addmonomial(K,indep,K):-!.
- aux_addmonomial(0,_,0):-!.
- aux_addmonomial(1,XExp,XExp):-!.
- aux_addmonomial(K,XExp,K*XExp).
-
- submonomial(-K1,K2,K3):-
-   number(K1),number(K2),!,
-   K3 is -(K1+K2).
- submonomial(K1,K2,K3):-
-  number(K1),number(K2),!,
-  K3 is K1-K2.
- submonomial(M1,M2,M3):-
-  monparts(M1,K1,XExp),
-  monparts(M2,K2,XExp),
-  K3 is K1-K2,
-  aux_submonomial(K3,XExp,M3).
+  K3 is -(K1+K2).
+submonomial(K1,K2,K3):-
+ number(K1),number(K2),!,
+ K3 is K1-K2.
+submonomial(M1,M2,M3):-
+ monparts(M1,K1,XExp),
+ monparts(M2,K2,XExp),
+ K3 is K1-K2,
+ aux_submonomial(K3,XExp,M3).
 
 
- aux_submonomial(K,indep,K):-!.
- aux_submonomial(0,_,0):-!.
- aux_submonomial(1,XExp,XExp):-!.
- aux_submonomial(K,XExp,K*XExp).
+aux_submonomial(K,indep,K):-!.
+aux_submonomial(0,_,0):-!.
+aux_submonomial(1,XExp,XExp):-!.
+aux_submonomial(K,XExp,K*XExp).
 
-%transforma uma lista num polinomio
-list2poly([P],P):-monomial(P), !.
-list2poly([P|L1], M+P):-list2poly(L1,M),!.
 
-%simplifica uma lista de monomios
-%simpoly_list(V,X):- list2poly(V,Y), simpoly(Y,Z), poly2list(Z,X).
-simpoly_list([],[]).
-simpoly_list([X|L], L2) :- add_poly2list(X, L, 0, L3), simpoly_list(L3, L2),!.
-simpoly_list([X|L], [R|L2]) :- simmon(X, X1), add_poly2list(X1, L, R, L3), simpoly_list(L3,L2), !.
+ %transforma uma lista num polinomio
+ list2poly([P],P):-monomial(P), !.
+ list2poly([P|L1], M+P):-list2poly(L1,M),!.
 
-add_poly2list(X, [], X, []).
-add_poly2list(X, [Y|L1], R1, L2) :- addmonomial(X, Y, R), add_poly2list(R, L1, R1, L2), !.
-add_poly2list(X, [Y|L1], R, [Y|L2]) :- add_poly2list(X,L1,R,L2),!.
+ %simplifica uma lista de monomios
+ %simpoly_list(V,X):- list2poly(V,Y), simpoly(Y,Z), poly2list(Z,X).
+ simpoly_list([],[]).
+ simpoly_list([X|L], L2) :- add_poly2list(X, L, 0, L3), simpoly_list(L3, L2),!.
+ simpoly_list([X|L], [R|L2]) :- simmon(X, X1), add_poly2list(X1, L, R, L3), simpoly_list(L3,L2), !.
 
-%soma de dois polinomios
-addpoly(X,Y,Z):- poly2list(X+Y,L), simpoly_list(L,T), list2poly(T,Z), !.
+ add_poly2list(X, [], X, []).
+ add_poly2list(X, [Y|L1], R1, L2) :- addmonomial(X, Y, R), add_poly2list(R, L1, R1, L2), !.
+ add_poly2list(X, [Y|L1], R, [Y|L2]) :- add_poly2list(X,L1,R,L2),!.
 
-scalelist(0, _, [0]).
-scalelist(N, [X|[]], [R]):-
-    number(N),
-    scalemonomial(X,N,R),!.
-scalelist(N, [X|T], [R|T2]):-
-    number(N),
-    scalemonomial(X,N,R),
-    scalelist(N, T, T2),!.
+ %soma de dois polinomios
+ addpoly(X,Y,Z):- poly2list(X+Y,L), simpoly_list(L,T), list2poly(T,Z), !.
 
-scalemonomial(K1,K2,K3):-
-    number(K1),number(K2),!,K3 is K1*K2.
-scalemonomial(M1,M2,M3):-
-    monomial(M1),
-    monparts(M1,K1,XExp),
-    number(M2),
-    K3 is K1*M2,
-    aux_addmonomial(K3,XExp,M3).
+ scalelist(0, _, [0]).
+ scalelist(N, [X|[]], [R]):-
+     number(N),
+     scalemonomial(X,N,R),!.
+ scalelist(N, [X|T], [R|T2]):-
+     number(N),
+     scalemonomial(X,N,R),
+     scalelist(N, T, T2),!.
 
-%multiplica um numero a um polinomio
-scalepoly(P, N, NP):- poly2list(P, L), scalelist(N, L, NL), list2poly(NL, NP),!.
+ scalemonomial(K1,K2,K3):-
+     number(K1),number(K2),!,K3 is K1*K2.
+ scalemonomial(M1,M2,M3):-
+     monomial(M1),
+     monparts(M1,K1,XExp),
+     number(M2),
+     K3 is K1*M2,
+     aux_addmonomial(K3,XExp,M3).
+
+ %multiplica um numero a um polinomio
+ scalepoly(P, N, NP):- poly2list(P, L), scalelist(N, L, NL), list2poly(NL, NP),!.
--- trabalho2.pl
+++ trabalho2.pl
@@ -1,70 +1,59 @@
-/*
-Exemplos e input que podemos ter:
-  show two plus x squared
-  > 2 + x^2
-
-  multiply three by two plus x squared
-  > 3*(2+x^2) ou seja 6+3*x^2
-
-  simplify two plus two plus one times y
-  > 2+2+1*y ou seja 4+y
-
-  show two plus x squared as P1
-  > P1=2+x^2
-
-  multiply three by P1(2+x^2)
-  > 3*P1 ou seja 6+2*x^2
-
-  multiply three by P1 as P2
-  > P2 = 3*P1
-
-  add P1 with x raised to 3
-  > P1 + x^3 ou seja 2+x^2+x^3
-
-  show stored polynomials
-  > P1 = 2+x^2
-    P2 = 6+3*x^2
-
-  forget P1 and show stored polynomials
-  > P2 = 6 + 3*x^2
-
-% Read N diagnoses and retrieve them in a list
-diagnosis(N, Diags) :-
-    diagnosis(N, [], D),
-    reverse(D, Diags).  % Assuming you want them listed in the order they were read
-
-diagnosis(N, A, Diags) :-
-    N > 0,
-    rw_diag(Diag),      % read and write one diagnosis
-    A1 = [Diag|A],
-    N1 is N - 1,
-    diagnosis(N1, A1, Diags).
-diagnosis(0, A, A).
-
-rw_diag(Diag) :-
-    readln(Diag),
-    write(Diag), nl.
-*/
-
 :-[trabalho1].
 
-%iniciamento do programa
+nb_linkval(p1, 0).
+nb_linkval(p2, 0).
+nb_linkval(p3, 0).
+nb_linkval(p4, 0).
+nb_linkval(p5, 0).
+
+
+
+%iniciaçao do programa
 polyplay :-
-    write("Hello there...."), nl,
+    write("---Welcome---"), nl,
+    polyplay2.
+
+polyplay2 :-
     read(X),
     split_string(X, " ", "", L),
     string_list_to_atom(L, LA),
     option(LA, B), B is 1, nl,
-    polyplay.
+    polyplay2.
 
-polyplay :- write("Error, something is not right, you dumb fuck :^) "). %caso erro, imprime menssagem de erro
+%polyplay2 :- write("Error, something went bad!!"), polyplay2. %caso erro, imprime menssagem de erro
 
-shw_opt(L) :- remove_ele(by, L, NL), atoms_to_numbers(NL, [], P), atomic_list_concat(P, "", NP), write(NP).
-mul_opt([X|T]) :- remove_ele(by, T, NL), atoms_to_numbers(NL, [], P), atomic_list_concat(P, "", NP), atom_to_number(X, Num), term_string(K, NP), scalepoly(K, Num, NNP), write(NNP).
-add_opt([X|T]) :- remove_ele(with, T, NL).
+%definiçao das opçoes
+shw_opt(L) :- remove_ele(by, L, NL),
+  atoms_to_numbers(NL, [], P),
+  atomic_list_concat(P, "", NP),
+  write(NP).
+shw_opt(X) :- global_variavels(X, [G], []), nb_getval(X, Val), term_string(K, G), format('~w=~w~n', [K, Val]). 
+
+mul_opt([X|T]) :-
+  remove_ele(by, T, NL),
+  atoms_to_numbers(NL, [], P),
+  atomic_list_concat(P, "", NP),
+  atom_to_number(X, Num),
+  term_string(K, NP),
+  scalepoly(K, Num, NNP),
+  write(NNP).
+
+sim_opt(L) :-
+  atoms_to_numbers(L, [], P),
+  atomic_list_concat(P, "", NP),
+  term_string(K, NP),
+  simpoly(K, Poly),
+  write(Poly).
+
+add_opt([X|T]) :- remove_ele(with, T, NL), atoms_to_numbers(L, [], P), atomic_list_concat(P, "", NP), nb_setval(X, NP).
+
+while_not_op([X], [X]) :- unidade(X, _, []), !.
+while_not_op([X|T], [X|T2]) :- unidade(X, _, []), while_not_op(T, T2), !.
+while_not_op([X|T], []) :- simbol(X, _, []), !.
+while_not_op([X|T], []) :- variable(X, _, []), !.
 
 %conversao um atom para um numero
-atom_to_number(A, N) :- units(A, [N], []), !.
+atom_to_number(A, N) :- unidade(A, [N], []), !.
 
 %remove all intancies of that element X from the List and return a new list
 remove_ele(_, [], []):-!.
@@ -72,7 +61,9 @@ remove_ele(X, [X|T], L):- remove_ele(X, T, L), !.
 remove_ele(X, [H|T], [H|L]):- remove_ele(X, T, L ), !.
 
 %converçao do texto num polinomio
-%converter os atoms em numeros e simbulos e polos numa lista de atoms, depois concata-se tudo e fica numa exprressao ====> IDEIA
+%converter os atoms em numeros e simbulos e polos numa lista de atoms, depois concata-se tudo e fica numa exprressao
+
+%atoms_to_numbers([X|[Y]], RPoly, P) :- square(X, Y, O), append(L, RPoly, Poly2), reverse(Poly2, P),!.
 atoms_to_numbers([X], RPoly, P) :- units(X, L, []), append(L, RPoly, Poly2), reverse(Poly2, P),!.
 atoms_to_numbers([X], RPoly, P) :- simbol(X, L, []), append(L, RPoly, Poly2), reverse(Poly2, P),!.
 atoms_to_numbers([X], RPoly, P) :- variable(X, L, []), append(L, RPoly, Poly2),reverse(Poly2, P), !.
@@ -81,12 +72,14 @@ atoms_to_numbers([X|T], Poly, P) :- units(X, L, []), append(L, Poly, Poly2), ato
 atoms_to_numbers([X|T], Poly, P) :- simbol(X, L, []), append(L, Poly, Poly2), atoms_to_numbers(T, Poly2, P), !.
 atoms_to_numbers([X|T], Poly, P) :- variable(X, L, []), append(L, Poly, Poly2), atoms_to_numbers(T, Poly2, P), !.
 
+
 % se tiver a palavra show,multiply, add, simplify como input entao vamos fazer a conversao de palavras para numeros
 option([X|T], 1) :- X = show, shw_opt(T).
 option([X|T], 1) :- X = multiply, mul_opt(T).
-option([X|T], 1) :- X = add ,add_opt(T).
-option([X|_], 1) :- X = simplify.
-option([X|_], 0).
+option([X|T], 1) :- X = add , add_opt(T).
+option([X|T], 1) :- X = simplify, sim_opt(T).
+option([X|_], 1) :- X = bye, write("Goodbye, friend"), nl, abort().
+option([_|_], 0).
 
 
 %converte uma string para uma lista de atoms
@@ -94,32 +87,23 @@ string_list_to_atom([X], [A]) :- string_to_atom(X, A),!.
 string_list_to_atom([X|L1], [A|L2]) :- string_to_atom(X, A), string_list_to_atom(L1, L2),!.
 
 
-polynomial(P+M) --> polynomial(P), plus, monomial(M), !.
-polynomial(P-M) --> polynomial(P), minus, monomial(M), !.
-polynomial(M)   --> monomial(M), !.
-polynomial(-M)  --> minus, monomial(M), !.
+number(L, N) :- list_number(L, T), sumlist(T, N).
 
-monomial(V)     --> variable(V);
-		    coefficient(V).
-monomial(V^P)   --> variable(V), raise, power(P), !.
-monomial(C*V)   --> coefficient(C), mult, variable(V), !.
-monomial(C*V)   --> number(C), mult, variable(V).
-monomial(C*V^P) --> coefficient(C), mult, variable(V), raise, power(P),!.
+list_number([X], [Y]) :- unidade(X, [Y], []), !.
+list_number([X|T], [Y|T2]) :- unidade(X, [Y], []), list_number(T, T2), !.
 
-
-plus  --> [plus].
-minus --> [minus].
-mult  --> [times].
-raise --> [raised],[to].
-
-zero(Z) --> [zero], {Z = 0}.
+unidade(W, L, []) :- units(W, L, []).
+unidade(W, L, []) :- til20(W, L, []).
+unidade(W, L, []) :- tens(W, L, []).
 
 simbol(S) --> [+], {S = plus},!;
               [-], {S = minus},!;
               [*], {S = times},!;
-              [^], {S = raised_to}.
+              [^], {S = raised_to},!.
 
-units(C) --> [1],    {C = one},!;
+
+units(C) --> [0],    {C = zero},!;
+             [1],    {C = one},!;
 	           [2],    {C = two},!;
 	           [3],    {C = three},!;
 	           [4],    {C = four},!;
@@ -142,27 +126,18 @@ til20(C) --> [10],    {C = ten},!;
 
 tens(C)  --> [20],   {C = twenty},!;
 	           [30],   {C = thirty},!;
-	           [40],   {C = forty},!;
+	           [40],   {C = fourty},!;
 	           [50],   {C = fifty},!;
 	           [60],   {C = sixty},!;
 	           [70],   {C = seventy},!;
              [80],   {C = eighty},!;
 	           [90],   {C = ninety}.
 
-hundreds --> [hundred].
-thousands --> [thousand].
-millions --> [million];[millions].
-
-/*
-test --> (zero; units; tens).
-test --> (units; tens), test2.
-test2  --> hundreds; thousands; millions.
-*/
-global_variavels(GV) --> ['P1'], {GV = 'P1'}, !;
-                         ['P2'], {GV = 'P2'}, !;
-                         ['P3'], {GV = 'P3'}, !;
-                         ['P4'], {GV = 'P4'}, !;
-                         ['P5'], {GV = 'P5'}.
+global_variavels(GV) --> ['P1'], {GV = p1}, !;
+                         ['P2'], {GV = p2}, !;
+                         ['P3'], {GV = p3}, !;
+                         ['P4'], {GV = p4}, !;
+                         ['P5'], {GV = p5}.
 
 
 variable(V) --> [a], {V = a},!;
@@ -192,12 +167,24 @@ variable(V) --> [a], {V = a},!;
 		            [y], {V = y},!;
 		            [z], {V = z}.
 
-number(L, N) :- list_number(L, T), sumlist(T, N).
 
-%[twenty, five] -----> [20, 5]
-list_number([X], [Y]) :- unidade(X, [Y], []), !.
-list_number([X|T], [Y|T2]) :- unidade(X, [Y], []), list_number(T, T2), !.
 
-unidade(W, L, []) :- units(W, L, []).
-unidade(W, L, []) :- til20(W, L, []).
-unidade(W, L, []) :- tens(W, L, []).
+/*
+polynomial(P+M) --> polynomial(P), plus, monomial(M), !.
+polynomial(P-M) --> polynomial(P), minus, monomial(M), !.
+polynomial(M)   --> monomial(M), !.
+polynomial(-M)  --> minus, monomial(M), !.
+
+monomial(V)     --> variable(V);
+coefficient(V).
+monomial(V^P)   --> variable(V), raise, power(P), !.
+monomial(C*V)   --> coefficient(C), mult, variable(V), !.
+monomial(C*V)   --> number(C), mult, variable(V).
+monomial(C*V^P) --> coefficient(C), mult, variable(V), raise, power(P),!.
+
+
+plus  --> [plus].
+minus --> [minus].
+mult  --> [times].
+raise --> [raised],[to].
+*/
